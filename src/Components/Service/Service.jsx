431cc7cc03d40a9da69d99cd91d436fa5acaf608
iammrk24748 <masudvairus@gmail.com>
2024-06-09 20:35:14 +0600

    update service

--- src/Components/Service/Service.jsx
+++ src/Components/Service/Service.jsx
@@ -27,7 +27,7 @@ const Service = () => {
             </h2>
             <div className="bg-[#06223F] h-2 md:h-4 w-24 md:w-32"></div>
           </div>
-          <h2 className="text-[#06223f31] font-bold text-3xl md:text-5xl -mt-2 uppercase">
+          <h2 className="text-[#06223f31] font-bold text-6xl md:text-9xl-mt-2 uppercase">
             My Services
           </h2>
         </div>
@@ -41,96 +41,21 @@ const Service = () => {
               delay={0}
               play={true}
               direction="right"
+              className="flex gap-5"
             >
-              <div className="flex justify-center items-center gap-5 py-5">
-                <FaHtml5 className="text-3xl md:text-5xl font-bold text-[#06223f31] ml-5" />
-                <h2 className="text-3xl md:text-5xl font-bold text-[#06223f31] ">
-                  HTML
-                </h2>
-              </div>
-              <div className="flex justify-center items-center gap-5">
-                <FaCss3Alt className="text-3xl md:text-5xl font-bold text-[#06223f31] ml-5" />
-                <h2 className="text-3xl md:text-5xl font-bold text-[#06223f31] ">
-                  CSS
-                </h2>
-              </div>
-              <div className="flex justify-center items-center gap-5">
-                <RiTailwindCssFill className="text-4xl md:text-5xl font-bold text-[#06223f31] ml-5" />
-                <h2 className="text-3xl md:text-5xl font-bold text-[#06223f31] ">
-                  Tailwind CSS
-                </h2>
-              </div>
-              <div className="flex justify-center items-center gap-5">
-                <FaBootstrap className="text-3xl md:text-5xl font-bold text-[#06223f31] ml-5" />
-                <h2 className="text-3xl md:text-5xl font-bold text-[#06223f31] ">
-                  Bootstrap
-                </h2>
-              </div>
-              <div className="flex justify-center items-center gap-5">
-                <IoLogoJavascript className="text-3xl md:text-5xl font-bold text-[#06223f31] ml-5" />
-                <h2 className="text-3xl md:text-5xl font-bold text-[#06223f31] ">
-                  JavaScript
-                </h2>
-              </div>
-              <div className="flex justify-center items-center gap-5">
-                <SiTypescript className="text-3xl md:text-5xl font-bold text-[#06223f31] ml-5" />
-                <h2 className="text-3xl md:text-5xl font-bold text-[#06223f31] ">
-                  Typescript
-                </h2>
-              </div>
-              <div className="flex justify-center items-center gap-5">
-                <FaReact className="text-3xl md:text-5xl font-bold text-[#06223f31] ml-5" />
-                <h2 className="text-3xl md:text-5xl font-bold text-[#06223f31] ">
-                  React Js
-                </h2>
-              </div>
-              <div className="flex justify-center items-center gap-5">
-                <TbBrandRedux className="text-3xl md:text-5xl font-bold text-[#06223f31] ml-5" />
-                <h2 className="text-3xl md:text-5xl font-bold text-[#06223f31] ">
-                  Redux Js
-                </h2>
-              </div>
-            </Marquee>
-          </div>
-
-          <div className="">
-            <Marquee
-              gradient={false}
-              speed={80}
-              delay={0}
-              play={true}
-              direction="left"
-            >
-              <div className="flex justify-center items-center gap-5">
-                <FaNodeJs className="text-3xl md:text-5xl font-bold text-[#06223f31] ml-5" />
-                <h2 className="text-3xl md:text-5xl font-bold text-[#06223f31] ">
-                  Node Js
-                </h2>
-              </div>
-              <div className="flex justify-center items-center gap-5">
-                <SiExpress className="text-3xl md:text-5xl font-bold text-[#06223f31] ml-5" />
-                <h2 className="text-3xl md:text-5xl font-bold text-[#06223f31] ">
-                  Express Js
-                </h2>
-              </div>
-              <div className="flex justify-center items-center gap-5 py-5">
-                <DiMongodb className="text-3xl md:text-5xl font-bold text-[#06223f31] ml-5" />
-                <h2 className="text-3xl md:text-5xl font-bold text-[#06223f31] ">
-                  MongoDB
-                </h2>
-              </div>
-              <div className="flex justify-center items-center gap-5">
-                <FaWordpress className="text-3xl md:text-5xl font-bold text-[#06223f31] ml-5" />
-                <h2 className="text-3xl md:text-5xl font-bold text-[#06223f31] ">
-                  Wordpress
-                </h2>
-              </div>
-              <div className="flex justify-center items-center gap-5">
-                <FaPhp className="text-3xl md:text-5xl font-bold text-[#06223f31] ml-5" />
-                <h2 className="text-3xl md:text-5xl font-bold text-[#06223f31] ">
-                  Php
-                </h2>
-              </div>
+              <FaHtml5 className="text-6xl md:text-9xl font-bold text-[#06223f31] ml-5" />
+              <FaCss3Alt className="text-6xl md:text-9xl font-bold text-[#06223f31] ml-5" />
+              <RiTailwindCssFill className="text-6xl md:text-9xl font-bold text-[#06223f31] ml-5" />
+              <FaBootstrap className="text-6xl md:text-9xl font-bold text-[#06223f31] ml-5" />
+              <IoLogoJavascript className="text-6xl md:text-9xl font-bold text-[#06223f31] ml-5" />
+              <SiTypescript className="text-6xl md:text-9xl font-bold text-[#06223f31] ml-5" />
+              <FaReact className="text-6xl md:text-9xl font-bold text-[#06223f31] ml-5" />
+              <TbBrandRedux className="text-6xl md:text-9xl font-bold text-[#06223f31] ml-5" />
+              <FaNodeJs className="text-6xl md:text-9xl font-bold text-[#06223f31] ml-5" />
+              <SiExpress className="text-6xl md:text-9xl font-bold text-[#06223f31] ml-5" />
+              <DiMongodb className="text-6xl md:text-9xl font-bold text-[#06223f31] ml-5" />
+              <FaWordpress className="text-6xl md:text-9xl font-bold text-[#06223f31] ml-5" />
+              <FaPhp className="text-6xl md:text-9xl font-bold text-[#06223f31] ml-5" />
             </Marquee>
           </div>
         </div>
